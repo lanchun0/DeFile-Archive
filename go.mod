4575973c5f83ad20c828baff3c9f23703a729d75
lanchun0 <e0722762@u.nus.edu>
2022-02-10 14:12:43 +0800

    master

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module dfa
 
 go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.15
--- server.go
+++ server.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	common "github.com/ethereum/go-ethereum/common"
+	ethclient "github.com/ethereum/go-ethereum/ethclient"
+)
 
 func main() {
-	fmt.Println("This is the first version!")
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	account := common.HexToAddress("0x560F1Fd3ea56DfBd44dde2724a9258ecb9f6EC98")
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 100000000000000000000
+
+	blockNumber := big.NewInt(0)
+	balanceAt, err := client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balanceAt) // 100000000000000000000
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balanceAt.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue) // 100
+
+	pendingBalance, err := client.PendingBalanceAt(context.Background(), account)
+	fmt.Println(pendingBalance) // 25729324269165216042
 }
