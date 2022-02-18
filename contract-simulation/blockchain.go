14a14d8321eabc672db77e2b03b5f9ad63cba7c0
lanchun0 <e0722762@u.nus.edu>
2022-02-18 12:34:45 +0800

    master

--- contract-simulation/blockchain.go
+++ contract-simulation/blockchain.go
@@ -3,6 +3,7 @@ package contractsimulation
 import (
 	"database/sql"
 	"dfa/entity"
+	"fmt"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -24,6 +25,9 @@ type BehaviorContract interface {
 	Authorize(hostkey, customerkey, fileid string, p entity.Permission) (bool, error)
 }
 
+type datachain struct {
+	DB *sql.DB
+}
 type behaviorchain struct {
 	DB *sql.DB
 }
@@ -32,9 +36,32 @@ func NewBehaviorChain() BehaviorContract {
 	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
 	db, err := sql.Open("mysql", path)
 	if err != nil {
+		fmt.Println("behaviorchain: faled to open sql")
 		panic(err)
 	}
+	if err := db.Ping(); err != nil {
+		fmt.Println("behaviorchain: faled to ping")
+		panic(err)
+	}
+	fmt.Println("behaviorchain: connection succeeds")
 	return &behaviorchain{
+		DB: db,
+	}
+}
+
+func NewDataChain() DataContract {
+	path := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8"}, "")
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		fmt.Println("datachain: faled to open sql")
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		fmt.Println("datachain: faled to ping")
+		panic(err)
+	}
+	fmt.Println("datachain: connection succeeds")
+	return &datachain{
 		DB: db,
 	}
 }
