52f7048f9528efe1c36e4ac24be32f482f058ed0
lanchun0 <e0722762@u.nus.edu>
2022-02-24 21:17:16 +0800

    master

---
+++ general/ipfs.go
@@ -0,0 +1,122 @@
+package general
+
+import (
+	"context"
+	"fmt"
+	config "github.com/ipfs/go-ipfs-config"
+	files "github.com/ipfs/go-ipfs-files"
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	"github.com/ipfs/go-ipfs/core/node/libp2p"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+	icore "github.com/ipfs/interface-go-ipfs-core"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+//Part 1 - Getting an IPFS node running: To get get a node running as an ephemeral node (that will cease to exist when the run ends)
+
+//Prepare and set up the plugins
+
+func setupPlugins(externalPluginsPath string) error {
+	// Load any external plugins if available on externalPluginsPath
+	plugins, err := loader.NewPluginLoader(filepath.Join(externalPluginsPath, "plugins"))
+	if err != nil {
+		return fmt.Errorf("error loading plugins: %s", err)
+	}
+
+	// Load preloaded and external plugins
+	if err := plugins.Initialize(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	if err := plugins.Inject(); err != nil {
+		return fmt.Errorf("error initializing plugins: %s", err)
+	}
+
+	return nil
+}
+
+//Create an IPFS repo
+
+func createTempRepo(ctx context.Context) (string, error) {
+	repoPath, err := ioutil.TempDir("", "ipfs-shell")
+	if err != nil {
+		return "", fmt.Errorf("failed to get temp dir: %s", err)
+	}
+
+	// Create a config with default options and a 2048 bit key
+	cfg, err := config.Init(ioutil.Discard, 2048)
+	if err != nil {
+		return "", err
+	}
+
+	// Create the repo with the config
+	err = fsrepo.Init(repoPath, cfg)
+	if err != nil {
+		return "", fmt.Errorf("failed to init ephemeral node: %s", err)
+	}
+
+	return repoPath, nil
+}
+
+//Construct the IPFS node instance itself
+
+func createNode(ctx context.Context, repoPath string) (icore.CoreAPI, error) {
+	//Creates an IPFS node and returns its coreAPI
+	// Open the repo
+	repo, err := fsrepo.Open(repoPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct the node
+
+	nodeOptions := &core.BuildCfg{
+		Online:  true,
+		Routing: libp2p.DHTOption, // This option sets the node to be a full DHT node (both fetching and storing DHT Records)
+		// Routing: libp2p.DHTClientOption, // This option sets the node to be a client DHT node (only fetching records)
+		Repo: repo,
+	}
+
+	node, err := core.NewNode(ctx, nodeOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	// Attach the Core API to the constructed node
+	return coreapi.NewCoreAPI(node)
+}
+
+//Part 2 - Adding a file and a directory to IPFS
+
+//Prepare the file to be added to IPFS
+func getUnixfsFile(path string) (files.File, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	st, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := files.NewReaderPathFile(path, file, st)
+	if err != nil {
+		return nil, err
+	}
+
+	return f, nil
+}
+
+//Add a file to IPFS -- main()
+
+//Part 3 - Getting the file and directory you added back
+
+//Get the file back -- main()
+
+//Write the file to local filesystem -- main()
--- go.mod
+++ go.mod
@@ -5,15 +5,14 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/google/go-cmp v0.5.4 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/uuid v1.3.0
-	github.com/ipfs/go-ipfs-api v0.3.0
-	github.com/kr/text v0.2.0 // indirect
+	github.com/ipfs/go-ipfs v0.12.0
+	github.com/ipfs/go-ipfs-config v0.19.0
+	github.com/ipfs/go-ipfs-files v0.0.9
+	github.com/ipfs/interface-go-ipfs-core v0.5.2
+	github.com/libp2p/go-libp2p-core v0.12.0
+	github.com/libp2p/go-libp2p-peerstore v0.6.0
+	github.com/multiformats/go-multiaddr v0.4.1
 	github.com/tpkeeper/gin-dump v1.0.1
-	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210816183151-1e6c022a8912 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
