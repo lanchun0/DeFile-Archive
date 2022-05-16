a8013c7daac643430d321775732dacc696f3e0cc
lanchun0 <e0722762@u.nus.edu>
2022-05-16 16:09:30 +0800

    master

--- controller/user-controller.go
+++ controller/user-controller.go
@@ -1,8 +1,9 @@
 package controller
 
 import (
-	"dfa/dto"
 	"dfa/service"
+	"log"
+	"strconv"
 
 	"fmt"
 	"net/http"
@@ -10,6 +11,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// POST (no token)
+// no params
 func (c *dfaController) Register(ctx *gin.Context) {
 	user, priv, err := c.contract.Register()
 	if err != nil {
@@ -25,19 +28,13 @@ func (c *dfaController) Register(ctx *gin.Context) {
 	})
 }
 
-// post
+// POST (no token)
+// param: privatekey string
 func (c *dfaController) Login(ctx *gin.Context) {
-	var credentials dto.Credentials
-	err := ctx.ShouldBind(&credentials)
-	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "expired identity",
-		})
-		return
-	}
-	//fmt.Println(credentials.PrivateKey)
-	b, err := c.contract.Login(credentials.PrivateKey)
+	// var credentials dto.Credentials
+	// err := ctx.ShouldBind(&credentials)
+	priv := ctx.DefaultPostForm("privatekey", "N.A.")
+	b, err := c.contract.Login(priv)
 	if err != nil {
 		fmt.Println(err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{
@@ -45,7 +42,7 @@ func (c *dfaController) Login(ctx *gin.Context) {
 		})
 		return
 	}
-	token := c.jWtService.GenerateToken(credentials.PrivateKey)
+	token := c.jWtService.GenerateToken(priv)
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"msg":   "success login",
 		"user":  b,
@@ -53,30 +50,34 @@ func (c *dfaController) Login(ctx *gin.Context) {
 	})
 }
 
+// POST(token)
+// param: amount uint
 func (c dfaController) Topup(ctx *gin.Context) {
-	var topupRequest dto.TopupRequest
-	err := ctx.ShouldBind(&topupRequest)
-	if err != nil {
-		fmt.Println(err)
+	// var topupRequest dto.TopupRequest
+	amountStr := ctx.DefaultPostForm("amount", "0")
+	errFunc := func(err error) {
+		log.Println(err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "invalid top up parameters",
+			"msg": "failed to top up",
 		})
+	}
+	amount, err := strconv.Atoi(amountStr)
+	if err != nil || amount <= 0 {
+		if amount <= 0 {
+			err = fmt.Errorf("invalid amount: %s", amountStr)
+		}
+		errFunc(err)
 		return
 	}
-	priv, err := service.ParseToken(topupRequest.Token)
+	token := ctx.GetHeader("Authorization")
+	priv, err := service.ParseToken(token)
 	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "invalid identity",
-		})
+		errFunc(err)
 		return
 	}
-	ok, tx, err := c.contract.Topup(priv, topupRequest.Amount)
+	ok, tx, err := c.contract.Topup(priv, uint64(amount))
 	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "failed to toup",
-		})
+		errFunc(err)
 		return
 	}
 	msg := "topup status: "
@@ -87,22 +88,31 @@ func (c dfaController) Topup(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"msg":         msg,
-		"amount":      topupRequest.Amount,
+		"amount":      amount,
 		"transaction": tx,
 	})
 }
 
+// POST(token)
+// param: amount uint
 func (c dfaController) WithDraw(ctx *gin.Context) {
-	var withdraw dto.WithDrawRequest
-	err := ctx.ShouldBind(&withdraw)
-	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "invalid top up parameters",
+	amountStr := ctx.DefaultPostForm("amount", "0")
+	errFunc := func(err error) {
+		log.Println(err)
+		ctx.JSON(http.StatusNotModified, gin.H{
+			"msg": "failed to withdraw",
 		})
+	}
+	amount, err := strconv.Atoi(amountStr)
+	if err != nil || amount <= 0 {
+		if amount <= 0 {
+			err = fmt.Errorf("invalid amount: %s", amountStr)
+		}
+		errFunc(err)
 		return
 	}
-	priv, err := service.ParseToken(withdraw.Token)
+	token := ctx.GetHeader("Authorization")
+	priv, err := service.ParseToken(token)
 	if err != nil {
 		fmt.Println(err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{
@@ -110,12 +120,9 @@ func (c dfaController) WithDraw(ctx *gin.Context) {
 		})
 		return
 	}
-	ok, tx, err := c.contract.WithDraw(priv, withdraw.Amount)
+	ok, tx, err := c.contract.WithDraw(priv, uint64(amount))
 	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "failed to withdraw",
-		})
+		errFunc(err)
 		return
 	}
 	msg := "withdraw status: "
@@ -126,30 +133,37 @@ func (c dfaController) WithDraw(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"msg":         msg,
-		"amount":      withdraw.Amount,
+		"amount":      amount,
 		"transaction": tx,
 	})
 }
 
+// POST(token)
+// param: amount uint
 func (c dfaController) Approve(ctx *gin.Context) {
-	var approve dto.ApproveRequest
-	err := ctx.ShouldBind(&approve)
-	if err != nil {
-		fmt.Println(err)
+	amountStr := ctx.DefaultPostForm("amount", "0")
+	errFunc := func(err error) {
+		log.Println(err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "invalid top up parameters",
+			"msg": "failed to approve",
 		})
+	}
+	amount, err := strconv.Atoi(amountStr)
+	if err != nil || amount <= 0 {
+		if amount <= 0 {
+			err = fmt.Errorf("invalid amount: %s", amountStr)
+		}
+		errFunc(err)
 		return
 	}
-	priv, err := service.ParseToken(approve.Token)
+	token := ctx.GetHeader("Authorization")
+	priv, err := service.ParseToken(token)
 	if err != nil {
-		fmt.Println(err)
-		ctx.JSON(http.StatusUnauthorized, gin.H{
-			"msg": "invalid identity",
-		})
+		errFunc(err)
 		return
 	}
-	ok, tx, err := c.contract.Approve(priv, approve.Price)
+
+	ok, tx, err := c.contract.Approve(priv, uint64(amount))
 	if err != nil {
 		fmt.Println(err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{
@@ -165,7 +179,7 @@ func (c dfaController) Approve(ctx *gin.Context) {
 	}
 	ctx.JSON(http.StatusAccepted, gin.H{
 		"msg":         msg,
-		"price":       approve.Price,
+		"price":       amount,
 		"transaction": tx,
 	})
 }
