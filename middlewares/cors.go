a6f6124bd444ba9866af3ccefb1fb43ab0c3ec41
lanchun0 <e0722762@u.nus.edu>
2022-05-28 22:36:51 +0800

    master

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,22 +1,25 @@
 package middlewares
 
 import (
-	"time"
+	"net/http"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
-		cors.New(cors.Config{
-			AllowAllOrigins: true,
-			AllowMethods:    []string{"*"},
-			AllowHeaders:    []string{"Origin"},
-			ExposeHeaders:   []string{"Content-Length", "Authorization"},
-			// AllowCredentials: true,
-			MaxAge: 12 * time.Hour,
-		})
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
 	}
+
 }
